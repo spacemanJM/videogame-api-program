02bc1dbdec1f9a54ff3b2b137ac8d01093c3e2cc
Your Name <you@example.com>
2025-07-28 14:57:18 -0500

    Changed CORS to fix fetch issue

--- server.js
+++ server.js
@@ -10,17 +10,7 @@ app.use(express.static("public"));
 const cors = require("cors");
 const allowedOrigins = ["http://localhost:5500", "http://127.0.0.1:5500"];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps, Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.get("/api/games", async(req, res) => {
     const searchQuery = req.query.search;
